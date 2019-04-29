a011c85146916cd78e0551287d85048ddfa536ec
pr0n00gler <pr0n00gler@MacBook-Pro-Mihail.local>
2019-04-29 11:13:49 +0300

    test's fixes

--- consensus/dkg_test.go
+++ consensus/dkg_test.go
@@ -1,7 +1,6 @@
 package consensus
 
 import (
-	"crypto/rand"
 	"sort"
 	"testing"
 
@@ -9,6 +8,7 @@ import (
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/types"
+	"go.dedis.ch/kyber/pairing/bn256"
 )
 
 func createDKGMsg(addr []byte, roundID int, data []byte, toIndex, numEntities int) DKGDataMessage {
@@ -30,19 +30,19 @@ func TestDKGDataSignable(t *testing.T) {
 		expected, signBytes []byte
 		err                 error
 	)
-	msg := createDKGMsg(randomBytes(10), 1, randomBytes(10), 1, 1)
+	testAddr := []byte("some_test_address")
+	testData := []byte("some_test_data")
+
+	msg := createDKGMsg(testAddr, 1, testData, 1, 1)
 
 	signBytes = msg.Data.SignBytes()
 
 	msg.Data.Signature = nil
 	if expected, err = cdc.MarshalBinaryLengthPrefixed(msg.Data); err != nil {
-		panic(err)
+		t.Error(err.Error())
+		return
 	}
 	require.Equal(t, expected, signBytes, "Got unexpected sign bytes for DKGData.")
-}
-
-func TestDKGDataVerifySignature(t *testing.T) {
-
 }
 
 func TestDKGPKStoreFindByAddress(t *testing.T) {
@@ -52,26 +52,8 @@ func TestDKGPKStoreFindByAddress(t *testing.T) {
 	N := 1000
 	testKeys := make([]PK2Addr, 0, N)
 	for i := 0; i < N; i++ {
-		testKeys = append(testKeys, PK2Addr{crypto.Address(randomBytes(5)), nil}) // PK is nil because it's not needed for testing this func
-		pkStore.Add(&testKeys[i])
-	}
-
-	sort.Sort(pkStore)
-
-	for _, v := range testKeys {
-		found, err := pkStore.FindByAddress(v.Addr.String())
-		require.NoError(t, nil, err)
-		require.Equal(t, v.Addr, found.Addr)
-	}
-}
-
-func BenchmarkDKGPKStoreFindByAddress(t *testing.B) {
-	var (
-		pkStore PKStore
-	)
-	testKeys := make([]PK2Addr, 0, t.N)
-	for i := 0; i < t.N; i++ {
-		testKeys = append(testKeys, PK2Addr{crypto.Address(randomBytes(5)), nil}) // PK is nil because it's not needed for testing this func
+		suite := bn256.NewSuiteG2()
+		testKeys = append(testKeys, PK2Addr{crypto.Address([]byte{byte(i)}), suite.Point().Pick(suite.RandomStream())})
 		pkStore.Add(&testKeys[i])
 	}
 
@@ -98,17 +80,12 @@ func TestDKGVerifyMessage(t *testing.T) {
 	//let's pretend another peer's dealer gets pubKey and saves it
 	dealer.pubKeys.Add(&PK2Addr{crypto.Address(pubkey.Address().Bytes()), dealer.pubKey})
 
-	msg := createDKGMsg(randomBytes(10), 1, randomBytes(10), 1, 1)
+	testAddr := []byte("some_test_address")
+	testData := []byte("some_test_data")
+
+	msg := createDKGMsg(testAddr, 1, testData, 1, 1)
 
 	require.NoError(t, nil, dealer.Sign(msg.Data))
 
 	require.NoError(t, nil, dealer.VerifyMessage(msg))
-
-}
-
-//randomBytes only for testing
-func randomBytes(n int) []byte {
-	rb := make([]byte, n)
-	rand.Read(rb)
-	return rb
 }
