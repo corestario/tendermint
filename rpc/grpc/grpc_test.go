994258c37f04a9f3d19c4c04a9f3a7cc00e82ff7
rybnov <ryban92@gmail.com>
2019-05-13 16:54:56 +0300

    Add old files from dcr branch

---
+++ rpc/grpc/grpc_test.go
@@ -0,0 +1,36 @@
+package core_grpc_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/tendermint/tendermint/types"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/tendermint/tendermint/abci/example/kvstore"
+	core_grpc "github.com/tendermint/tendermint/rpc/grpc"
+	rpctest "github.com/tendermint/tendermint/rpc/test"
+)
+
+func TestMain(m *testing.M) {
+	// start a tendermint node in the background to test against
+	app := kvstore.NewKVStoreApplication()
+	node := rpctest.StartTendermint(app)
+	node.ConsensusState().SetVerifier(&types.MockVerifier{})
+	code := m.Run()
+
+	// and shut down proper at the end
+	node.Stop()
+	node.Wait()
+	os.Exit(code)
+}
+
+func TestBroadcastTx(t *testing.T) {
+	require := require.New(t)
+	res, err := rpctest.GetGRPCClient().BroadcastTx(context.Background(), &core_grpc.RequestBroadcastTx{Tx: []byte("this is a tx")})
+	require.Nil(err, "%+v", err)
+	require.EqualValues(0, res.CheckTx.Code)
+	require.EqualValues(0, res.DeliverTx.Code)
+}
