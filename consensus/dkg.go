51c2656481de6c6c3bc270c9e54caa488282b058
programmer10110 <programmer10110@yandex.ru>
2019-05-14 18:38:10 +0300

    little fix

--- consensus/dkg.go
+++ consensus/dkg.go
@@ -49,7 +49,7 @@ func (cs *ConsensusState) handleDKGShare(mi msgInfo) {
 	dealer, ok := cs.dkgRoundToDealer[msg.RoundID]
 	if !ok {
 		cs.Logger.Info("DKG: dealer not found, creating a new dealer", "round_id", msg.RoundID)
-		dealer = NewDKGDealer(cs.Validators, cs.privValidator, cs.sendDKGMessage, cs.Logger, msg.RoundID-1)
+		dealer = NewDKGDealer(cs.Validators, cs.privValidator, cs.sendDKGMessage, cs.Logger, msg.RoundID)
 		cs.dkgRoundToDealer[msg.RoundID] = dealer
 		if err := dealer.start(); err != nil {
 			common.PanicSanity(fmt.Sprintf("failed to start a dealer (round %d): %v", cs.dkgRoundID, err))
@@ -126,7 +126,7 @@ func (cs *ConsensusState) startDKGRound() error {
 	dealer, ok := cs.dkgRoundToDealer[cs.dkgRoundID]
 	if !ok {
 		cs.Logger.Info("DKG: dealer not found, creating a new dealer", "round_id", cs.dkgRoundID)
-		dealer = NewDKGDealer(cs.Validators, cs.privValidator, cs.sendDKGMessage, cs.Logger, cs.dkgRoundID-1)
+		dealer = NewDKGDealer(cs.Validators, cs.privValidator, cs.sendDKGMessage, cs.Logger, cs.dkgRoundID)
 		cs.dkgRoundToDealer[cs.dkgRoundID] = dealer
 		return dealer.start()
 	}
@@ -181,7 +181,7 @@ type DKGDealer struct {
 	losers []crypto.Address
 }
 
-func NewDKGDealer(validators *types.ValidatorSet, privValidator types.PrivValidator, sendMsgCb func(*types.DKGData), logger log.Logger, previousRound int) *DKGDealer {
+func NewDKGDealer(validators *types.ValidatorSet, privValidator types.PrivValidator, sendMsgCb func(*types.DKGData), logger log.Logger, startRound int) *DKGDealer {
 	return &DKGDealer{
 		validators:    validators,
 		addrBytes:     privValidator.GetPubKey().Address().Bytes(),
@@ -191,14 +191,13 @@ func NewDKGDealer(validators *types.ValidatorSet, privValidator types.PrivValida
 		suiteG1:       bn256.NewSuiteG1(),
 		suiteG2:       bn256.NewSuiteG2(),
 
-		roundID: previousRound,
+		roundID: startRound,
 
 		deals: make(map[string]*dkg.Deal),
 	}
 }
 
 func (m *DKGDealer) start() error {
-	m.roundID++
 	m.secKey = m.suiteG2.Scalar().Pick(m.suiteG2.RandomStream())
 	m.pubKey = m.suiteG2.Point().Mul(m.secKey, nil)
 	m.generateTransitions()
